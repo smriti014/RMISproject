3ec230b223e0afa0f0684ac0c61ad2ab5d5ec09f
smriti <smritiprasad014@gmail.com>
2024-12-13 19:32:49 +0530

    project added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './component/style.css'
+import Vmp from './component/vmp';
+
+
+
+function App() {
+  return (
+    <>
+     <Vmp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/vmp.jsx
@@ -0,0 +1,41 @@
+import "./vmp.css"
+
+
+const Vmp = () => {
+    return (
+        <>
+            <div className="hexa-container">
+                <div className="row">
+                    <div className="hexa-border">
+                        <div className="hexa">
+                            <div className="title">Vision</div>
+                            <div className="info">
+                                <p>To empower businesses with innovative IT solutions that drive growth, efficiency, and transformation.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="hexa-border">
+                        <div className="hexa">
+                            <div className="title">Mission</div>
+                            <div className="info">
+                                <p>To deliver cutting-edge technology and client-centric services that solve real-world challenges.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hexa-border">
+                        <div className="hexa">
+                            <div className="title">Policy</div>
+                            <div className="info">
+                                <p>Innovation, integrity, collaboration, and a commitment to exceeding client expectations.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Vmp
